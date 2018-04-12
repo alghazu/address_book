05211ddf01fe05914d17d0b81932ea919aa65b97
limei <limei@evolve.se>
2018-04-12 11:39:04 +0200

    first_step

--- features/step_definitions/basic_steps.js
+++ features/step_definitions/basic_steps.js
@@ -1,7 +1,11 @@
-const {Given, Then} = require("cucumber");
-Given('I visit the site', function () {
-  // Write code here that turns the phrase above into concrete actions
-  return 'pending';
+const {After, Given, Then, When} = require("cucumber");
+
+After(async function(){
+  return await this.closeHomePage()
+})
+
+Given('I visit the site', async function () {
+  return await this.openHomePage()
 });
 
 Then('I should see {string}', function (string) {
@@ -13,3 +17,23 @@ Then('I should see {string}', function (string) {
   // Write code here that turns the phrase above into concrete actions
   return 'pending';
 });
+When('I click {string}', async function(string) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending'
+})
+ 
+Then('I fill in {string} with {string}', async function(string, string2) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending'
+})
+ 
+Then('I should have {int} contact in my address book', async function(int) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending'
+})
+ 
+Then('I should not see {string}', async function(string) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending'
+})
+ 
---
+++ features/support/world.js
@@ -0,0 +1,22 @@
+const { setWorldConstructor } = require('cucumber');
+const {expect } = require('chai');
+const  puppeteer  = require ( 'puppeteer');
+
+const HOME_PAGE = 'http://localhost:3000';
+
+class AddressBookWorld {
+  constructor() {}
+
+  //Open the home page using puppeteer
+  //async/await
+  async openHomepage(){
+    this.browser = await puppeteer.launch();
+    this.page = await this.browser.newPage();
+    await this.page.goto(HOME_PAGE);
+  }
+  async closeHomePage() {
+    await this.browser.close();
+  }
+}
+
+setWorldConstructor(AddressBookWorld);
